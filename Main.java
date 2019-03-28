63cb3f4edab1122e8ffe17c31ed20a1ac24f65e8
hong-phong <46840800+hong-phong@users.noreply.github.com>
2019-03-28 20:04:22 +0700

    ADF_2

--- Main.java
+++ Main.java
@@ -1,30 +1,59 @@
-package Practical;
+package Exam_ADF2;
 
 import java.util.Scanner;
 
 public class Main {
+    static ManagementSystem ms = new ManagementSystem();
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        showMenu();
+    }
 
-            Item a;
-
-            System.out.println("itemId: ");
-            int itemId = scanner.nextInt();
-            scanner.nextLine();
-
-            System.out.println("name: ");
-            String name = scanner.nextLine();
-
-            System.out.println("Producer: ");
-            String producer = scanner.nextLine();
-
-            System.out.println("price: ");
-            double price = scanner.nextDouble();
-            scanner.nextLine();
-
-           a = new Item(itemId,name,producer,price);
-
-            System.out.println("Item details: "+ a.toString());
+    public static void showMenu() {
+        System.out.println("Airline Reservation System");
+        System.out.println("1. Add reservation");
+        System.out.println("2. Modify passenger");
+        System.out.println("3. Display passenger");
+        System.out.println("4. Exit");
+        Scanner scanner = new Scanner(System.in);
+        String opt = scanner.nextLine();
+        switch (opt) {
+            case "1":
+                ms.add(input());
+                showMenu();
+                break;
+            case "2":
+                ms.update();
+                showMenu();
+                break;
+            case "3":
+                ms.display();
+                showMenu();
+                break;
+            case "4":
+                return;
+            default:
+                System.out.println("Wrong option:");
+                showMenu();
+                break;
         }
+    }
+    public static Reservation  input() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ReservationID: ");
+        int id = scanner.nextInt();
+        System.out.println("PassengerName: ");
+        scanner.nextLine();
+        String name = scanner.nextLine();
+        System.out.println("Address ");
+        String address = scanner.nextLine();
+        System.out.println("Phone: ");
+        String phone = scanner.nextLine();
+        System.out.println("Date departure: ");
+        String date_departure = scanner.nextLine();
+        System.out.println("Date return: ");
+        String date_return = scanner.nextLine();
+        return new Reservation(id, name, address, phone, date_departure, date_return);
+    }
 }
