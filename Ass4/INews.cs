5113ace80f621b39f28d9ddaf3ef2b81fd9e4edd
hong-phong <46840800+hong-phong@users.noreply.github.com>
2019-05-25 19:00:23 +0700

    Add files via upload

---
+++ Ass4/INews.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ass4
+{
+    public interface INews
+    {
+        void Display();
+    }
+}
